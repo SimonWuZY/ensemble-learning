c57c0a161dd798c7769f6ce5bcd8df4e00861b97
Simon_Wzy <1691663479@qq.com>
2024-12-17 21:06:17 +0800

    homePage

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,13 @@
 import Link from "next/link"
+import { Navbar } from "./navbar"
 
 
 const Home = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center">
+    <div className="min-h-screen flex flex-col">
+      <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white">
+        <Navbar />
+      </div>
       Click <Link href="/documents/123">
         <span className="text-blue-500 underline">&nbsp;here&nbsp;</span>
       </Link> go to documentid
--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -21,12 +21,14 @@ import StarterKit from '@tiptap/starter-kit'
 import { useEditorStore } from '@/store/use-editor-store'
 import { FontSizeExtension } from "@/extensions/font-size";
 import { LineHeightExtension } from "@/extensions/line-height";
+import { Ruler } from './ruler';
 
 export const Editor = () => {
     // 状态管理 
     const { setEditor } = useEditorStore();
 
     const editor = useEditor({
+        immediatelyRender: false,
         // 每个不同的状态都进行更新
         onCreate({ editor }) {
             setEditor(editor);
@@ -88,24 +90,12 @@ export const Editor = () => {
             }),
             TaskList,
         ],
-        content: `<table>
-          <tbody>
-            <tr>
-              <th>Name</th>
-              <th colspan="3">Description</th>
-            </tr>
-            <tr>
-              <td>Cyndi Lauper</td>
-              <td>Singer</td>
-              <td>Songwriter</td>
-              <td>Actress</td>
-            </tr>
-          </tbody>
-        </table>`,
+        content: ``,
     })
 
     return (
         <div className='size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible'>
+            <Ruler />
             <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
                 <EditorContent editor={editor} />
             </div>
