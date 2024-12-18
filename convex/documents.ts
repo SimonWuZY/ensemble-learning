f071f00ac6d3854d874d55c982e4bf0f36b7d755
Simon_Wzy <1691663479@qq.com>
2024-12-18 23:03:26 +0800

    add: document-create

--- convex/documents.ts
+++ convex/documents.ts
@@ -1,4 +1,22 @@
-import { query } from "./_generated/server";
+import { ConvexError, v } from "convex/values";
+import { mutation, query } from "./_generated/server";
+
+export const createDocument = mutation({
+    args: { title: v.optional(v.string()), initialContent: v.optional(v.string()) },
+    handler: async (ctx, args) => {
+        const user = await ctx.auth.getUserIdentity();
+
+        if (!user) {
+            throw new ConvexError("Unathorized");
+        }
+
+        return await ctx.db.insert("documents", {
+            title: args.title ?? "未命名的文档",
+            ownerId: user.subject,
+            initialContent: args.initialContent,
+        });
+    },
+})
 
 export const getDocuments = query({
     handler: async (ctx) => {
--- src/app/(home)/templates-gallrey.tsx
+++ src/app/(home)/templates-gallrey.tsx
@@ -1,5 +1,7 @@
 "use client"
-
+import { useMutation } from "convex/react";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 import {
     Carousel,
     CarouselContent,
@@ -9,10 +11,24 @@ import {
 } from "@/components/ui/carousel";
 import { templates } from "@/constants/templates";
 import { cn } from "@/lib/utils";
+import { api } from "../../../convex/_generated/api";
 
 
 export const TemplatesGallery = () => {
-    const isCreating = false;
+    const router = useRouter();
+    const createDocument = useMutation(api.documents.createDocument);
+    const [isCreating, setIsCreating] = useState(false);
+
+    const onTemplateClick = (title: string, initialContent: string) => {
+        setIsCreating(true);
+        createDocument({ title, initialContent })
+            .then((documentId) => {
+                router.push(`/documents/${documentId}`);
+            })
+            .finally(() => {
+                setIsCreating(false);
+            })
+    }
 
     return (
         <div className="bg-[#F1F3F4]">
@@ -35,7 +51,7 @@ export const TemplatesGallery = () => {
                                 >
                                     <button
                                         disabled={isCreating}
-                                        onClick={() => { }}
+                                        onClick={() => onTemplateClick(template.label, "")}
                                         style={{
                                             backgroundImage: `url(${template.imageUrl})`,
                                             backgroundSize: "cover",
