8db1b438b0162927832020e1f57ca7b1883e4485
Simon_Wzy <1691663479@qq.com>
2024-12-18 09:34:49 +0800

    search component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -5,12 +5,15 @@ import { Navbar } from "./navbar"
 const Home = () => {
   return (
     <div className="min-h-screen flex flex-col">
-      <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white">
+      <div className="fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4">
         <Navbar />
       </div>
-      Click <Link href="/documents/123">
-        <span className="text-blue-500 underline">&nbsp;here&nbsp;</span>
-      </Link> go to documentid
+      <div className="mt-16">
+        Click <Link href="/documents/123">
+          <span className="text-blue-500 underline">&nbsp;here&nbsp;</span>
+        </Link> go to documentid
+      </div>
+
     </div>
   )
 }
