216918bc945426daecca8a9f62037ba2d27193ae
Simon_Wzy <1691663479@qq.com>
2024-12-18 19:10:41 +0800

    test

--- convex/schema.ts
+++ convex/schema.ts
@@ -7,5 +7,12 @@ export default defineSchema({
         initialContent: v.optional(v.string()),
         ownerId: v.string(),
         roomId: v.optional(v.string()),
+        organizationId: v.optional(v.string()),
     })
+        .index("by_owner_id", ["ownerId"])
+        .index("by_orgainization_id", ["organizationId"])
+        .searchIndex("search_title", {
+            searchField: "title",
+            filterFields: ["ownerId", "organizationId"],
+        }),
 });
